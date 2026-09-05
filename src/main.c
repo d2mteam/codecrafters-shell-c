c44e9c54559eadc2c79ed8deaa53f8c87e6cbec1
Minh <23020110@vnu.edu.vn>
2026-09-06 05:36:09 +0700

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
-
+  char input[100];
+  fgets(input, 100, stdin);
+  input[strlen(input) - 1] = '\0';
+  printf("%s: command not found\n", input);
   return 0;
 }
